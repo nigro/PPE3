e4b4284b7d89424e5061db63037dfbbf807a89a9
nigro <nicolas.grondin.95@gmail.com>
2015-09-25 15:11:17 +0200

    1modifPB

--- class/page_base.class.php
+++ class/page_base.class.php
@@ -146,7 +146,7 @@ class page_base {
 			<footer>
 				<p>Site de la crèche des petits bouts de Choux -  ANGERS </p>
 				<p id="copyright">
-				créé par _____________________________________________
+				créé par GRONDIN Nicolas et BAUMARD Alban (les plus beaux)
 				SSII 1FO@SIO - technologies mises en oeuvre PHP objet - jquery - Ajax
 				<a href="http://chevrollier.e-lyco.fr/">Chevrollier</a> 
 				</p>
